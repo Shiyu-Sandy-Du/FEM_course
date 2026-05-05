17e9630257759a193986dda21d7640d830c98ffa
Shiyu-Sandy-Du <shiyud@kth.se>
2026-05-05 15:07:34 +0200

    add convergence for LLF

--- dissipation_dispersion_LLF/error_converge_Legendre.py
+++ dissipation_dispersion_LLF/error_converge_Legendre.py
@@ -143,55 +143,102 @@ def track_eigenvalue_branch(p, xi_vals, branch='physical'):
 # Example
 # ============================================================
 if __name__ == "__main__":
-    lam_list = []
-    p = 2
-    xi = 0.2
+    for p in [1,2]:
+        # xi = 0.2
 
-    x, w, D, A1, B1, IM1, A1T, B1T = build_local_matrices(p)
-    G1 = build_G1(p, xi)
-    eigs = np.linalg.eigvals(G1)
+        # x, w, D, A1, B1, IM1, A1T, B1T = build_local_matrices(p)
+        # G1 = build_G1(p, xi)
+        # eigs = np.linalg.eigvals(G1)
 
-    np.set_printoptions(precision=6, suppress=True)
+        # np.set_printoptions(precision=6, suppress=True)
 
-    print("GLL nodes x =")
-    print(x)
-    print("\nGLL weights w =")
-    print(w)
-    print("\nDifferentiation matrix D =")
-    print(D)
+        # print("GLL nodes x =")
+        # print(x)
+        # print("\nGLL weights w =")
+        # print(w)
+        # print("\nDifferentiation matrix D =")
+        # print(D)
 
-    print("\nA1 =")
-    print(A1)
-    print("\nB1 =")
-    print(B1)
-    print("\nIM1 =")
-    print(IM1)
+        # print("\nA1 =")
+        # print(A1)
+        # print("\nB1 =")
+        # print(B1)
+        # print("\nIM1 =")
+        # print(IM1)
 
-    print("\nG1(xi) =")
-    print(G1)
+        # print("\nG1(xi) =")
+        # print(G1)
 
-    print("\nEigenvalues of G1 =")
-    print(eigs)
+        # print("\nEigenvalues of G1 =")
+        # print(eigs)
 
-    # Track the physical branch over a range of xi
-    xi_vals = np.linspace(1e-6, np.pi, 100)
-    lam = track_eigenvalue_branch(p, xi_vals, branch="physical")
-    
+        # Track the physical branch over a range of xi
+        xi_vals = np.linspace(1e-4, np.pi*p, 1000)
+        lam_physical = track_eigenvalue_branch(p, xi_vals, branch="physical")
+        lam_list = []
+        for i in range(p+1):
+            lam = track_eigenvalue_branch(p, xi_vals, branch=i)
+            lam_list.append(lam)
 
-    ##########################################################
-    ### plot
-    import matplotlib.pyplot as plt
-    xi = np.linspace(0, np.pi, 100)
-    plt.figure(figsize=(5, 4))
-    plt.plot(xi, -lam.imag - xi, label=f'p={p}', markersize=4)
-    plt.plot(xi, xi**(p+2))
-    plt.scale("loglog")
-    # plt.plot(np.linspace(0, np.pi, 100), np.linspace(0, np.pi, 100), linestyle='--', color='gray')
-    plt.xlabel(r'$\xi$')
-    plt.ylabel(r'$-Imag(\lambda)$')
-    plt.xlim(0, np.pi)
-    # plt.xticks(np.linspace(0, np.pi, 5), ['0', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$'])
-    plt.title(f'Imaginary part of eigenvalues')
-    # plt.legend()
-    plt.grid()
-    plt.savefig('figures/tracked_branch_imag_Legendre_p5.png', dpi=300)
+        ##########################################################
+        ### plot
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(5, 5))
+        # for i in range(p+1):
+        #     lam = lam_list[i]
+        #     plt.plot(np.linspace(0, np.pi, 100), -lam.imag/p, label=f'branch {i}', markersize=4)
+        plt.plot(xi_vals/p, -lam_physical.imag/p, label=f'p={p}', color='red', markersize=4)
+        plt.plot(xi_vals/p, xi_vals/p, linestyle='--', color='gray')
+        plt.xlabel(r'$\xi$/P')
+        plt.ylabel(r'$-Imag(\lambda h)/P$')
+        plt.xlim(0, np.pi)
+        plt.xticks(np.linspace(0, np.pi, 5), ['0', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$'])
+        plt.title(f'Imaginary part of eigenvalues')
+        # plt.legend()
+        plt.grid()
+        plt.savefig(f'figures/tracked_branch_imag_Legendre_p{p}.png', dpi=300)
+
+        plt.figure(figsize=(5, 5))
+        # for i in range(p+1):
+        #     lam = lam_list[i]
+        #     plt.plot(np.linspace(0, np.pi, 100), lam.real/p, label=f'branch {i}', markersize=4)
+        plt.plot(xi_vals/p, lam_physical.real/p, label=f'p={p}', color='red', markersize=4)
+        plt.plot(xi_vals/p, [0 for _ in range(len(xi_vals))], linestyle='--', color='gray')
+        plt.xlabel(r'$\xi$/P')
+        plt.ylabel(r'$Real(\lambda h)$/P')
+        plt.xlim(0, np.pi)
+        plt.xticks(np.linspace(0, np.pi, 5), ['0', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$'])
+        plt.title(f'Real part of eigenvalues')
+        # plt.legend()
+        plt.grid()
+        plt.savefig(f'figures/tracked_branch_real_Legendre_p{p}.png', dpi=300)
+        
+        plt.figure(figsize=(6, 5))
+        plt.plot(np.log10(xi_vals), np.log10(abs(lam_physical.real)), label=f'p={p}', color='red', markersize=4)
+        if p == 1:
+            plt.plot(np.log10(xi_vals), (2*p + 2) * np.log10(xi_vals) - 3, linestyle='--', color='gray')
+        elif p == 2:
+            plt.plot(np.log10(xi_vals), (2*p + 2) * np.log10(xi_vals) - 5, linestyle='--', color='gray')
+        plt.xlabel(r'$log_{10}(\xi$)')
+        plt.ylabel(r'$log_{10}(abs(Real(\lambda h)))$)')
+        plt.xlim(np.log10(xi_vals[0]),np.log10(xi_vals[-1]))
+        plt.ylim(-17,0.5)
+        plt.title(f'Dispersion Error')
+        # plt.legend()
+        plt.grid()
+        plt.savefig(f'figures/dissipation_error_Legendre_p{p}.png', dpi=300)
+        
+        plt.figure(figsize=(6, 5))
+        plt.plot(np.log10(xi_vals), np.log10(abs(-lam_physical.imag - xi_vals)), label=f'p={p}', color='red', markersize=4)
+        if p == 1:
+            plt.plot(np.log10(xi_vals), (2*p + 3) * np.log10(xi_vals) - 3, linestyle='--', color='gray')
+        elif p == 2:
+            plt.plot(np.log10(xi_vals), (2*p + 3) * np.log10(xi_vals) - 5, linestyle='--', color='gray')
+        plt.xlabel(r'$log_{10}(\xi$)')
+        plt.ylabel(r'$log_{10}(abs(Imag(\lambda h)))$)')
+        plt.xlim(np.log10(xi_vals[0]),np.log10(xi_vals[-1]))
+        plt.ylim(-17,0.5)
+        plt.title(f'Dispersion Error')
+        # plt.legend()
+        plt.grid()
+        plt.savefig(f'figures/dispersion_error_Legendre_p{p}.png', dpi=300)
